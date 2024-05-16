1126c9ef1e38fc0d56c66f91872e54af1254101c
Afanti Afanti <magestore@outlook.com>
2024-05-16 01:45:59 +0000

    chore: code review

--- code.sh
+++ code.sh
@@ -1,2 +1,21 @@
-git clone --depth 1 https://github.com/bitcoin/bitcoin.git
-git clone --depth 1 https://github.com/prysmaticlabs/prysm.git
+#!/bin/bash
+
+# Array of repository URLs
+repositories=(
+  "https://github.com/bitcoin/bitcoin.git"
+  "https://github.com/prysmaticlabs/prysm.git"
+)
+
+# Clone or update repositories
+for repository in "${repositories[@]}"; do
+  # Extract repository name
+  repo_name=$(basename "$repository" .git)
+  
+  # Check if the repository already exists
+  if [ -d "$repo_name" ]; then
+    echo "Repository $repo_name already exists. Skipping..."
+  else
+    # Clone the repository
+    git clone --depth 1 "$repository"
+  fi
+done
