33513c59f1fce8cf3c8d3fe4715ac8e7e89b371a
Afanti Afanti <phpjavaphp@gmail.com>
2025-01-12 01:32:55 +0000

    feat: download content

---
+++ download.ts
@@ -0,0 +1,65 @@
+function my_button() {
+    var floatBtns = document.querySelectorAll('div[class^="index_floatBtn"]');
+    if (floatBtns.length > 0) {
+        floatBtns.forEach(button => {
+            button.click();
+        });
+    } else {
+        console.log('Float button is not found.');
+    }
+}
+
+function my_span() {
+  console.clear();
+  var spans = document.querySelectorAll('span');
+  var message = '';
+
+  function wordCount(str) {
+    return str.trim().split(/\s+/).length;
+  }
+
+  spans.forEach(span => {
+    if (wordCount(span.textContent) > 30) {
+      message += span.textContent + '\n';
+    }
+  });
+
+  if (message.trim() !== '') {
+    var blob = new Blob([message], { type: 'text/plain' });
+    var timestamp = new Date().toISOString().replace(/[^0-9]/g, '');
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = `span-${timestamp}.txt`;
+    link.click();
+  } else {
+    console.log('No spans with more than 30 words found.');
+  }
+}
+
+function scroll_div(distance, times) {
+    return new Promise((resolve) => {
+        const scrollElement = document.querySelector('div[class^="index_scrollWindow"]');
+        if (!scrollElement) {
+            console.error("Element not found");
+            resolve();
+            return;
+        }
+        let iterations = 0;
+        function scrollOnce() {
+            scrollElement.scrollTop += distance;
+            const scrollEvent = new Event('scroll');
+            scrollElement.dispatchEvent(scrollEvent);
+            iterations++;
+            if (iterations < times) {
+                setTimeout(scrollOnce, 500);
+            } else {
+                resolve();
+            }
+        }
+        scrollOnce();
+    });
+}
+
+async function my_scroll() {
+    await scroll_div(10000, 10);
+}
