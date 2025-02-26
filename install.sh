3073d1f5d6248ac6cbb16f3153df96280c7f0699
Afanti Afanti <phpjavaphp@gmail.com>
2025-02-26 00:11:50 +0000

    feat: crypto price

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
 sudo apt-get install ffmpeg
+sudo apt install -y protobuf-compiler
+pip install --upgrade numpy pillow yt-dlp gTTS
 pip install torch transformers moviepy manim
-pip install requests beautifulsoup4
+pip install elevenlabs requests beautifulsoup4
