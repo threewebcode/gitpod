7d95597fc99dd960149f0f03548672b77b43ef5e
Afanti Afanti <phpjavaphp@gmail.com>
2025-05-09 01:36:39 +0000

    chore: make updates

--- tokens.sh
+++ tokens.sh
@@ -1,3 +1,4 @@
+git clone --depth 1 https://github.com/block/goose.git
 git clone --depth 1 https://github.com/nottelabs/bua.git
 git clone --depth 1 https://github.com/nottelabs/notte.git
 # git clone --depth 1 https://github.com/vanta-cash/vanta.cash.git
