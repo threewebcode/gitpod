521e83cb0200374233f1c6550d40f3958f70dbff
Afanti Afanti <phpjavaphp@gmail.com>
2025-05-26 02:18:22 +0000

    chore: make updates

--- tokens.sh
+++ tokens.sh
@@ -1,3 +1,7 @@
+git clone --depth 1 https://github.com/ref-finance/guide.git
+git clone --depth 1 https://github.com/ref-finance/ref-contracts.git
+git clone --depth 1 https://github.com/traderjoe-xyz/joe-docs.git
+git clone --depth 1 https://github.com/traderjoe-xyz/joe-router.git
 git clone --depth 1 https://github.com/crypto-org-chain/chain-docs.git
 git clone --depth 1 https://github.com/crypto-org-chain/cronos-docs.git
 git clone --depth 1 https://github.com/crypto-org-chain/cronos.git
